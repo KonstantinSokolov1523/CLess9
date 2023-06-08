8e40f9c07ea6dfdde8463eff3723f6ad92cd16e7
KonstantinSokolov1523 <130489541+KonstantinSokolov1523@users.noreply.github.com>
2023-06-08 17:25:08 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,6 +32,7 @@ void task64()
 Console.Write("Введите число: ");
 int N = convert.ToInt32(Console.ReadLine());
 Console.Write($"N = {N} -> {LineDown(N)}");
+int result = 0;
 string LineDown(int N)
 {
 if(N == 1) return result.ToString();
