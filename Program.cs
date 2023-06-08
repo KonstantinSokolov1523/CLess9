8213af15a398c766f08201186557c9c5b427b9e7
VIRTUOSO <kostyakot817@gmail.com>
2023-06-08 23:08:50 +0500

    all tasks solved!

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
-Console.Write("Введите номер задачи: );
-int task = convert.ToInt32(Console.ReadLine());
-switch task
+﻿Console.Write("Введите номер задачи: ");
+int task = Convert.ToInt32(Console.ReadLine());
+
+switch (task)
 {
  case 64:
 {
-task64()
+task64();
+break;
 }
 case 66:
 {
-task66()
+task66();
+break;
 }
-case 68;
+case 68:
 {
-task68()
+task68();
+break;
 }
 default:
 {
 Console.WriteLine("Некорректный ввод(такой задачи нет)");
+break;
 }
 }
 
@@ -30,13 +35,12 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
 void task64()
 {
 Console.Write("Введите число: ");
-int N = convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 Console.Write($"N = {N} -> {LineDown(N)}");
-int result = 0;
 string LineDown(int N)
 {
-if(N == 1) return result.ToString();
-return result + LineDown(N-1);
+if(N == 1) return N.ToString();
+return N + ", " + LineDown(N-1);
 }
 }
 
@@ -49,14 +53,14 @@ M = 4; N = 8. -> 30 */
 void task66()
 {
 Console.Write("Введите начальное число: ");
-int min = convert.ToInt32(Console.ReadLine());
-Console.Write("Введите конечное число: );
-int max = convert.ToInt32(Console.readLine());
-Console.WriteLine(LineSumm(Min, Max));
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(LineSumm(min, max));
 int LineSumm(int M, int N)
 {
-if(M == N + 1) return result;
-return result + LineSumm(M+1);
+if(M == N) return M;
+return M + LineSumm(M+1, N);
 }
 }
 
@@ -65,20 +69,33 @@ return result + LineSumm(M+1);
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29 */
 
-void task68
+void task68()
 {
 Console.Write("Введите первое число число: ");
-int firstum = convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: );
-int secnum = convert.ToInt32(Console.readLine());
-if(firstnum>0 && secnum >0)
+int firstnum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secnum = Convert.ToInt32(Console.ReadLine());
+if(firstnum > 0 && secnum >0)
 {
-Console.Write($"m = {firstnum}, n = {secnum} A(m,n) = {Akker(firstnum, secnum)});
-int Akker(int f, int s)
-if(f == 0) return s + 1;
-if(f != 0 && s == 0) return Akker(f - 1, 1);
-if(f > 0 && s > 0) return Akker(f -1, Akker(f, s-1);
-return Akker(f, s,);
+    Console.Write($"m = {firstnum}, n = {secnum} A(m,n) = {Akker(firstnum, secnum)}");
+    int Akker(int f, int s)
+    {
+        if (f == 0)
+        {
+             return s + 1;
+        }
+        else if (s == 0 && f > 0)
+        {
+            return Akker(f - 1, 1);
+        }
+        else
+        {
+            return (Akker(f - 1, Akker(f, s - 1)));
+        }
+    }
 }
-else Console.WriteLine("Значения должны быть положительными");
+else
+{
+    Console.WriteLine("Введено отрицательное число(а)");
+}
 }
